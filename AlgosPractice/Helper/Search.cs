7c032888177971b89ae32ea36d635bcfba0efc5d
Purna Chandra Panda <pupanda@outlook.com>
2019-02-25 12:24:11 +0530

    Recursion corrected with return and added a comment on main program

--- AlgosPractice/Helper/Search.cs
+++ AlgosPractice/Helper/Search.cs
@@ -8,30 +8,36 @@ namespace AlgosPractice.Helper
 {
     static class Search
     {
-        //****** recursion does not yield correct results *********
-        //public static int GetMissingNumberInSequenceWithRecusion(int[] array, int l, int r)
-        //{
-        //    if(l <= r)
-        //    {
-        //        int m = (l + r) / 2;
+        /// <summary>
+        /// Get the missing number in ordered sequence
+        /// </summary>
+        /// <param name="array">input array</param>
+        /// <param name="l">lower index</param>
+        /// <param name="r">higher index</param>
+        /// <returns></returns>
+        public static int GetMissingNumberInSequenceWithRecusion(int[] array, int l, int r)
+        {
+            if (l <= r)
+            {
+                int m = (l + r) / 2;
 
-        //        if (array[m] - array[m - 1] != 1)
-        //        {
-        //            return array[m] - 1;
-        //        }
+                if (array[m] - array[m - 1] != 1)
+                {
+                    return array[m] - 1;
+                }
 
-        //        if (array[m] == m + 2)
-        //        {
-        //            GetMissingNumberInSequenceWithRecusion(array, l, m - 1);
-        //        }
-        //        else if (array[m] == m + 1)
-        //        {
-        //            GetMissingNumberInSequenceWithRecusion(array, m + 1, r);                    
-        //        }
-        //    }
+                if (array[m] == m + 2)
+                {
+                    return GetMissingNumberInSequenceWithRecusion(array, l, m - 1);
+                }
+                else if (array[m] == m + 1)
+                {
+                    return GetMissingNumberInSequenceWithRecusion(array, m + 1, r);
+                }
+            }
 
-        //    return -1;
-        //}
+            return -1;
+        }
 
         /// <summary>
         /// If input array is ordered sequence, this logic would help finish the job in O(logn). 
--- AlgosPractice/Program.cs
+++ AlgosPractice/Program.cs
@@ -12,8 +12,9 @@ namespace AlgosPractice
         {
             // lower bound example: 1, 2, 3, 5, 6, 7, 8, 9
             // higher bound example: 1, 2, 3, 4, 5, 7
-            var inputArray = new int[] { 1, 2, 3, 5, 6, 7, 8, 9};
-            var result = Helper.Search.GetMissingNumberInSequence(inputArray, 0, inputArray.Length - 1);
+            var inputArray = new int[] { 1, 2, 3, 4, 5, 7};
+            // call GetMissingNumberInSequenceWithRecusion or GetMissingNumberInSequence
+            var result = Helper.Search.GetMissingNumberInSequenceWithRecusion(inputArray, 0, inputArray.Length - 1);
             Console.WriteLine(result);
             Console.ReadLine();
         }
