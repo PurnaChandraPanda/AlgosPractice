77b9998db22b1a91371a268a37626c573428d302
Purna Chandra Panda <pupanda@outlook.com>
2019-02-25 12:07:19 +0530

    code comments updated

--- AlgosPractice/Helper/Search.cs
+++ AlgosPractice/Helper/Search.cs
@@ -37,9 +37,9 @@ namespace AlgosPractice.Helper
         /// If input array is ordered sequence, this logic would help finish the job in O(logn). 
         /// Else, follow one by one comparison in the whole array, which would take O(n) time.
         /// </summary>
-        /// <param name="array"></param>
-        /// <param name="l"></param>
-        /// <param name="r"></param>
+        /// <param name="array">input array</param>
+        /// <param name="l">lower index</param>
+        /// <param name="r">higher index</param>
         /// <returns></returns>
         public static int GetMissingNumberInSequence(int[] array, int l, int r)
         {
@@ -47,13 +47,16 @@ namespace AlgosPractice.Helper
 
             while (l <= r)
             {
+                // calculate middle value
                 m = (l + r) / 2;
 
+                // if in seqence, difference to previous number must be 1
                 if (array[m] - array[m - 1] != 1)
                 {
                     return array[m] - 1;
                 }
 
+                // update indices if to navigate lower or upper side
                 if (array[m] == m + 2)
                 {
                     r = m - 1;
@@ -61,7 +64,6 @@ namespace AlgosPractice.Helper
                 else if (array[m] == m + 1)
                 {
                     l = m + 1;
-
                 }
             }
 
